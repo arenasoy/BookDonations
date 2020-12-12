1516e937b04d547f2a06bb7943825cd025d26157
Carolina <caro.arenas2607@gmail.com>
2020-12-12 13:02:42 -0300

    select doacao

--- src/doacao/Doacao.java
+++ src/doacao/Doacao.java
@@ -1,6 +1,8 @@
 package doacao;
 
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 import bibliotecario.Bibliotecario;
 import livro.Livro;
@@ -11,6 +13,49 @@ public class Doacao {
 	private Instant dataHora;
 	private double pontuacao;
 	private Bibliotecario bibliotecario;
+
+	public Doacao(Livro livro, Instant dataHora, double pontuacao, Bibliotecario bibliotecario) throws Exception {
+
+		if (livro == null) {
+			throw new Exception("Livro é obrigatório");
+		}
+
+		if (dataHora == null) {
+			throw new Exception("Data é obrigatória");
+		}
+
+		if (pontuacao < 0) {
+			throw new Exception("Pontuação é obrigatória");
+		}
+
+		this.livro = livro;
+		this.dataHora = dataHora;
+		this.pontuacao = pontuacao;
+		this.bibliotecario = bibliotecario;
+	}
+
+	public Doacao(Instant dataHora, double pontuacao) {
+		this.dataHora = dataHora;
+		this.pontuacao = pontuacao;
+		this.bibliotecario = null;
+		this.livro = null;
+	}
+
+	public void print() {
+
+		System.out.println("Data: "
+				+ DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault()).format(dataHora)
+				+ " Pontuação: " + pontuacao);		
+		if (livro != null) {
+			System.out.println("Livro: ");
+			livro.print();
+		}
+		
+		if (bibliotecario != null) {
+			System.out.println("Bibliotecário");
+			bibliotecario.print();
+		}
+	}
 
 	public Livro getLivro() {
 		return livro;
--- src/doacao/DoacaoDAO.java
+++ src/doacao/DoacaoDAO.java
@@ -4,10 +4,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
+import bibliotecario.Bibliotecario;
 import conexao.Conexao;
+import livro.Livro;
+import livro.Origem;
 
 public class DoacaoDAO {
 
@@ -35,8 +39,7 @@ public class DoacaoDAO {
 			pstm = conn.prepareStatement(sql);
 			
 			pstm.setInt(1, doacao.getLivro().getCodigoBarras());
-			//TODO verify how to convert
-			//pstm.setDate(2, java.sql.Timestamp.valueOf(doacao.getDataHora()));
+			pstm.setTimestamp(2, Timestamp.from(doacao.getDataHora()));
 			pstm.setDouble(3, doacao.getPontuacao());
 			pstm.setInt(4, doacao.getBibliotecario().getCib());
 			
@@ -49,17 +52,43 @@ public class DoacaoDAO {
 
 	}
 
-	public List<Doacao> select() {
+	public List<Doacao> select(boolean selectLivro, boolean selectBibliotecario) {
 
 		List<Doacao> doacoes = new ArrayList<Doacao>();
 		try {
-			sql = "";
+			
+			StringBuilder sb = new StringBuilder();
+			sb.append("select * from doacao D");
+			
+			if (selectLivro) {
+				sb.append(" join livro_doador L on D.codigo_barras_ld = L.codigo_barras_ld");
+			}
+			
+			if (selectBibliotecario) {
+				sb.append(" left join bibliotecario B on D.bibliotecario_aprovador = B.cib");
+			}
+			
+			sql = sb.toString();
 			pstm = conn.prepareStatement(sql);
 
 			ResultSet rs = pstm.executeQuery();
 
 			while (rs.next()) {
 				
+				Doacao d = new Doacao(rs.getTimestamp("data_horario_doacao").toInstant(), rs.getDouble("pontuacao_doacao"));
+				
+				if (selectLivro) {
+					d.setLivro(new Livro(rs.getInt("codigo_barras"), rs.getString("autor"), rs.getString("titulo"),
+						rs.getInt("isbn"), rs.getString("edicao"), rs.getInt("condicao"),
+						Origem.valueOf(rs.getString("origem")), true));
+				}
+				
+				if (selectBibliotecario) {
+					d.setBibliotecario(new Bibliotecario(rs.getInt("cib"), rs.getString("senha_bibliotecario"),
+						rs.getString("nome_bibliotecario")));
+				}
+				
+				doacoes.add(d);
 			}
 			pstm.close();
 		} catch (SQLException e) {
